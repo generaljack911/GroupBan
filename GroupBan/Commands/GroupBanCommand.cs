93193397cda9a0f342fa088cb4d4852ba61d6c2b
jackson <Jackisontheattack06@gmail.com>
2021-06-08 14:40:45 -0500

    Add project files.

---
+++ GroupBan/Commands/GroupBanCommand.cs
@@ -0,0 +1,48 @@
+﻿using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBan.Commands
+{
+    class GroupBanCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public string Name => "groupban";
+
+        public string Help => "This group bans Groups that are online";
+
+        public string Syntax => "";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = UnturnedPlayer.FromName(command[0]);
+
+            if (player != null)
+            {
+                foreach (SteamPlayer otherplayer in Provider.clients)
+                {
+                    UnturnedPlayer sameplayer = UnturnedPlayer.FromSteamPlayer(otherplayer);
+
+                    if (sameplayer.SteamGroupID == player.SteamGroupID)
+                    {
+                        sameplayer.Ban("You were banned!", 999999999);
+
+
+                        Logger.Log($"{sameplayer.DisplayName} was banned!");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GroupBan/GroupBan.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBan
+{
+    public class GroupBan : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Logger.Log("GroupBan has loaded!");
+        }
+        protected override void Unload()
+        {
+            Logger.Log("GroupBan has unloaded!");
+        }
+    }
+}
